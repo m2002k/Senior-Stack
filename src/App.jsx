117027fb4f1cd630844326ff1b6a10351ca18588
Saudsaad5 <hason2002009@hotmail.com>
2025-04-18 14:42:11 +0300

    added a login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login'
 
 function App() {
   return(
-    <h1>Hello World</h1>
+    <Login />
   )
 }
 
