11d6ae86366a34d60eacd44ee4fd21c946f61297
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2025-04-30 22:01:45 +0300

    remove

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -18,7 +18,6 @@ import Calendar from "../components/Calendar2";
 import ManageTasks from "../components/ManageTasks";
 import ManageUsers from "../components/ManageUsers";
 import ManageTeams from "../components/ManageTeams";
-import Projects from "../components/Projects";
 
 function Dashboard() {
   const [userData, setUserData] = useState(null);
@@ -104,8 +103,6 @@ function Dashboard() {
         return <ManageTeams userData={userData} />;
       case 'manageTasks':
         return <ManageTasks userData={userData} />;
-      case 'projects':
-        return <Projects userData={userData} />;
       default:
         return <h2>Select a tab</h2>;
     }
